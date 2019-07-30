d5d8439f0735bb9ee173f76138d8ead162303126
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2019-07-29 21:22:03 -0700

    CONT-656: test module deleted

--- server/integration/testsuite/src/test/resources/mapper.js
+++
@@ -1,9 +0,0 @@
-// mode=mapper,reducer=reducer.js,collator=collator.js,language=javascript
-var unmarshalledValue = marshaller.objectFromByteBuffer(value)
-var words = unmarshalledValue.split(" ")
-for (var i=0; i < words.length; i++) {
-   var word = words[i];
-   if (word != null) {
-      collector.emit(words[i].toLowerCase(), 1);
-   }
-}
